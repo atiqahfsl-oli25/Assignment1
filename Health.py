dff0f4781c1e980eef8bf25a1ca6a95f3ed4c7d7
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 00:47:53 +0800

    Health.py

--- Health.py
+++ Health.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly.express as px 
 
 # --- Streamlit Page Configuration ---
 st.set_page_config(page_title="Health & Lifestyle Dashboard", layout="wide")
