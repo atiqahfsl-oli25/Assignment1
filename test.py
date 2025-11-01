9baee02301e7d87db6673a061a4b51e9032c4997
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 01:36:53 +0800

    test.py

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Online Learning Survey"
+    page_title="Online Learning Survey",
+    layout="wide"
 )
 
-page1 = st.Page('Health.py', title='Kepuasan Pelajar Dalam Pembelajaran', icon=":material/thumb_up_off_alt:")
+# Define your pages
+home = st.Page("home.py", title="Homepage", icon=":material/home:", default=True)
+page1 = st.Page("Health.py", title="Kepuasan Pelajar Dalam Pembelajaran", icon=":material/thumb_up_off_alt:")
 
-home = st.Page('Homepage.py', title='Homepage', default=True, icon=":material/home:")
+# Group pages into navigation
+pg = st.navigation({
+    "Menu": [home, page1, page2]
+})
 
-pg = st.navigation(
-        {
-            "Menu": [home, page1, page2]
-        }
-    )
-
+# Run navigation
 pg.run()
