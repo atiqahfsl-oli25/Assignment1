b40485056ce0e78e0eff3a2229a1e59dedde8aba
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 00:43:59 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Health and Lifestyle" 
+)
+
+visualise = st.Page('Health.py', title='Health Among Citizen', icon=":material/school:")
+
+home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
+
+pg = st.navigation(
+        {
+            "Menu": [home, visualise]
+        }
+    )
+
+pg.run()
