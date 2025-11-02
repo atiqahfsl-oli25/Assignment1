af7e9af27b2c11caf3ffc83efabe20634f25b666
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 12:32:05 +0800

    Update objective2.py

--- objective2.py
+++ objective2.py
@@ -7,6 +7,19 @@ st.title("💤 Objective 2: Sleep Pattern Analysis")
 url = 'https://raw.githubusercontent.com/atiqahfsl-oli25/Assignment1/refs/heads/main/dataframe.csv' 
 df = pd.read_csv(url)
 
-fig = px.box(df, x="Gender", y="Sleep Duration", color="Gender", title="Sleep Hours Distribution by Gender")
-st.plotly_chart(fig, use_container_width=True)
+st.subheader("🚬 2. Smoking Habit Distribution by Gender")
+fig2 = px.histogram(
+    df,
+    x="Smoking Habit",
+    color="Gender",
+    barmode="group",
+    title="Smoking Habit Distribution by Gender",
+    color_discrete_sequence=px.colors.qualitative.Pastel
+)
+fig2.update_layout(
+    xaxis_title="Smoking Habit",
+    yaxis_title="Number of Respondents",
+    legend_title="Gender"
+)
+st.plotly_chart(fig2, use_container_width=True)
 
