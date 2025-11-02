227f15f652723fa4aea82ead98dfeb17bfadd0fc
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 19:43:50 +0800

    Update objective3.py

--- objective3.py
+++ objective3.py
@@ -7,44 +7,6 @@ st.title("Objective 3: Food and Water Consumption")
 url = 'https://raw.githubusercontent.com/atiqahfsl-oli25/Assignment1/refs/heads/main/dataframe.csv' 
 df = pd.read_csv(url)
 
-st.title("🥗 1.Diet Type Distribution by Gender")
-df['Diet Type'] = df['Diet Type'].replace('nonveg', 'non veg')
-
-df_male = df[df['Gender'] == 'Male']
-df_female = df[df['Gender'] == 'Female']
-
-diet_counts_male = df_male['Diet Type'].value_counts()
-diet_counts_female = df_female['Diet Type'].value_counts()
-fig = go.Figure()
-# Male Pie
-fig.add_trace(go.Pie(
-    labels=diet_counts_male.index,
-    values=diet_counts_male.values,
-    name="Male",
-    hole=0.3,
-    domain={'x': [0, 0.45]},
-    textinfo='percent+label',
-    marker=dict(colors=['#66b3ff', '#99ff99', '#ffcc99', '#ff9999'])
-))
-# Female Pie
-fig.add_trace(go.Pie(
-    labels=diet_counts_female.index,
-    values=diet_counts_female.values,
-    name="Female",
-    hole=0.3,
-    domain={'x': [0.55, 1]},
-    textinfo='percent+label',
-    marker=dict(colors=['#ffb3e6', '#c2f0c2', '#ffcc99', '#ff9999'])
-))
-fig.update_layout(
-    title_text="Diet Type Distribution for Male and Female",
-    annotations=[
-        dict(text='Male', x=0.20, y=0.5, font_size=16, showarrow=False),
-        dict(text='Female', x=0.8, y=0.5, font_size=16, showarrow=False)
-    ]
-)
-st.plotly_chart(fig, use_container_width=True)
-
 st.subheader("💧 2.Average Water Intake per Day by Age Group")
 average_water_intake_by_age = df.groupby('Age Group')['Water Intake per Day'].mean().reset_index()
 fig = px.line(
