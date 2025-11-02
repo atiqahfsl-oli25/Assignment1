1d5395e151a2dcc4447fd3f66b1fc961ac26983e
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 10:04:29 +0800

    objective2.py

--- objective2.py
+++ objective2.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+
+def preprocess_data(df: pd.DataFrame):
+df = df.drop_duplicates()
+df = df.fillna(method='ffill').fillna(method='bfill')
+
+
+# Convert categorical columns to category type
+for col in df.select_dtypes(include=['object']).columns:
+df[col] = df[col].astype('category')
+
+
+return df
