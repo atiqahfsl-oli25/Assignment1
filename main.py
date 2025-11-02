c4f9fc2192705333761e18c3583584a85d9694ba
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 14:40:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ st.set_page_config(page_title="Healthcare and Lifestyle", layout="wide")
 
 # Pages
 home = st.Page("home.py", title="🏠 Home", default=True)
-objective1 = st.Page("objective1.py", title="🎓 Objective 1: Alcohol")
-objective2 = st.Page("objective2.py", title="🌾 Objective 2: Smoking Habit")
-objective3 = st.Page("objective3.py", title="🌱 Objective 3: Food and Water Consumption")
-objective4 = st.Page("objective4.py", title="🌱 Objective 4: Current Health Condition")
+objective1 = st.Page("objective1.py", title="Objective 1: Alcohol")
+objective2 = st.Page("objective2.py", title="Objective 2: Smoking Habit")
+objective3 = st.Page("objective3.py", title="Objective 3: Food and Water Consumption")
+objective4 = st.Page("objective4.py", title="Objective 4: Current Health Condition")
 
 # Navigation
 pg = st.navigation({
     "Main Menu": [home, objective1, objective2, objective3, objective4]
 })
+
 pg.run()
