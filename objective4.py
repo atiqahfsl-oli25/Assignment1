5d0c7cbb537bad9f8013c997d84bd8d04ba63895
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 12:15:54 +0800

    Update objective4.py

--- objective4.py
+++ objective4.py
@@ -7,6 +7,6 @@ st.title("🫀 Objective 4: Chronic Health Condition Insights")
 url = 'https://raw.githubusercontent.com/atiqahfsl-oli25/Assignment1/refs/heads/main/dataframe.csv' 
 df = pd.read_csv(url)
 
-fig = px.pie(df, names="Health_Condition", title="Distribution of Health Conditions")
+fig = px.pie(df, names="Current Health Conditions", title="Distribution of Health Conditions")
 st.plotly_chart(fig, use_container_width=True)
 
