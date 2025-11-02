ed34d6c014b4bfc70b9ca8239641a3627dabbdf6
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 15:22:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ home = st.Page("home.py", title="🏠 Home", default=True)
 objective1 = st.Page("objective1.py", title="Objective 1: Mental Health")
 objective2 = st.Page("objective2.py", title="Objective 2: Physical Health")
 objective3 = st.Page("objective3.py", title="Objective 3: Food and Water Consumption")
-objective4 = st.Page("objective4.py", title="Objective 4: Current Health Condition")
 
 # Navigation
 pg = st.navigation({
-    "Main Menu": [home, objective1, objective2, objective3, objective4]
+    "Main Menu": [home, objective1, objective2, objective3]
 })
 
 pg.run()
