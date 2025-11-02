2905d4f6832921728c905715c30f01638975796d
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 17:48:40 +0800

    Update objective1.py

--- objective1.py
+++ objective1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.title("Objective 1: Mental Health")
+st.title("Objective 1: Mental Health affect by Many Factors")
 
 url = 'https://raw.githubusercontent.com/atiqahfsl-oli25/Assignment1/refs/heads/main/dataframe.csv'  
 df = pd.read_csv(url)
