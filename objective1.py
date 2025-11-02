8b5e52399684b5d2cf7b8283b84a69e148a9cc10
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 10:30:46 +0800

    Update objective1.py

--- objective1.py
+++ objective1.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.title("🎯 Objective 1: Physical Activity Analysis")
 
-url = 
+url = 'https://raw.githubusercontent.com/atiqahfsl-oli25/Assignment1/refs/heads/main/dataframe.csv'  
 df = pd.read_csv(url)
 
 fig = px.histogram(df, x="Physical_Activity_Label", color="Gender", barmode="group", title="Physical Activity Level by Gender")
