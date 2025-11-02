00e0b7508a0d8d00295fa2a02c0d56d5c7a1b7a5
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 10:05:11 +0800

    objective3.py

--- objective3.py
+++ objective3.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import plotly.express as px
 
+
+def visualize_relationships(df):
+st.header("💤 Mental Health & Sleep Relationship")
+
+
+sleep_health_mean = df.groupby(['Sleep Issues', 'Gender'])['Mental Health Frequency'].mean().reset_index()
+fig = px.line(sleep_health_mean, x='Sleep Issues', y='Mental Health Frequency', color='Gender',
+markers=True, title='Mental Health Issue Frequency vs Sleep Issues',
+color_discrete_sequence=px.colors.qualitative.Set2)
+st.plotly_chart(fig, use_container_width=True)
