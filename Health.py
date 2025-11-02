de7c593be8824dfdfc8068ab8d40db77d3182359
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 11:51:49 +0800

    Update Health.py

--- Health.py
+++ Health.py
@@ -3,15 +3,15 @@ import streamlit as st
 st.set_page_config(page_title="Healthcare and Lifestyle", layout="wide")
 
 # Pages
-# home = st.Page("home.py", title="🏠 Home", default=True)
-objective1 = st.Page("objective1.py", title="🎓 Objective 1: Alcohol", default=True)
+home = st.Page("home.py", title="🏠 Home", default=True)
+objective1 = st.Page("objective1.py", title="🎓 Objective 1: Alcohol")
 objective2 = st.Page("objective2.py", title="🌾 Objective 2: Smoking Habit")
 objective3 = st.Page("objective3.py", title="🌱 Objective 3: Mental Health Issues vs Sleep Issues")
 objective4 = st.Page("objective4.py", title="🌱 Objective 4: Current Health Condition")
 
 # Navigation
 pg = st.navigation({
-    "Main Menu": [objective1, objective2, objective3, objective4]
+    "Main Menu": [home, objective1, objective2, objective3, objective4]
 })
 
 pg.run()
