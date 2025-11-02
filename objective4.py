590d190d130f2a2212a2b4e17085db71c571b4c0
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 10:05:46 +0800

    objective4.py

--- objective4.py
+++ objective4.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import plotly.express as px
 
+
+def visualize_health_conditions(df):
+st.header("👥 Average Health Conditions by Age Group")
+
+
+health_conditions_by_age = df.groupby('Age Group')['Current Health Conditions'].mean().reset_index()
+fig = px.line(health_conditions_by_age, x='Age Group', y='Current Health Conditions', markers=True,
+title='Average Current Health Conditions by Age Group',
+color_discrete_sequence=px.colors.qualitative.Safe)
+st.plotly_chart(fig, use_container_width=True)
