fe5f06a2b246fe71e11082a0ac9e8ec4b373330c
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 17:28:24 +0800

    Update objective3.py

--- objective3.py
+++ objective3.py
@@ -74,22 +74,25 @@ fig.update_layout(
 st.plotly_chart(fig, use_container_width=True)
 
 st.subheader("💧 Average Water Intake per Day by Age Group")
+
 average_water_intake_by_age = df.groupby('Age Group')['Water Intake per Day'].mean().reset_index()
 fig = px.line(
-    average_water_intake_by_age,
+    data_frame=average_water_intake_by_age,
     x='Age Group',
     y='Water Intake per Day',
     markers=True,
     title='Average Water Intake per Day by Age Group',
-    line_shape='linear',
-    color_discrete_sequence=['#1f77b4']
+    color_discrete_sequence=['#1f77b4']  # Blue tone
 )
 fig.update_layout(
-    xaxis_title="Age Group",
-    yaxis_title="Average Water Intake (litres)",
-    template="plotly_white",
-    hovermode="x unified"
+    xaxis_title='Age Group',
+    yaxis_title='Average Water Intake (litres)',
+    template='plotly_white',
+    hovermode='x unified'
 )
+fig.update_xaxes(tickangle=45)
+
 st.plotly_chart(fig, use_container_width=True)
+
 
 
