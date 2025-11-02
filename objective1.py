ad63c412bb07c9eadb7ecccc7035c8fc40d5b5a1
atiqahfsl-oli25 <atiqah.fsl@gmail.com>
2025-11-02 10:26:40 +0800

    Update objective1.py

--- objective1.py
+++ objective1.py
@@ -1,26 +1,11 @@
-# objective1.py
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.title("🎯 Objective 1: Physical Activity Analysis")
 
-def explore_data(df):
-    st.header("📈 Exploratory Data Analysis")
-    st.write("Preview of the dataset:")
-    st.dataframe(df.head())
+url = 
+df = pd.read_csv(url)
 
-
-    # Alcohol Consumption by Gender
-    st.subheader("🍷 Alcohol Consumption by Gender")
-    fig = px.histogram(df, x='Gender', color='Alcohol Consumption', barmode='group',
-                       title='Distribution of Alcohol Consumption by Gender',
-                       color_discrete_sequence=px.colors.qualitative.Set2)
-    st.plotly_chart(fig, use_container_width=True)
-
-
-    # Smoking Habit Distribution by Gender
-    st.subheader("🚬 Smoking Habit Distribution by Gender")
-    fig2 = px.histogram(df, x='Smoking Habit', color='Gender', barmode='group',
-                        title='Smoking Habit Distribution by Gender',
-                        color_discrete_sequence=px.colors.qualitative.Pastel)
-    st.plotly_chart(fig2, use_container_width=True)
+fig = px.histogram(df, x="Physical_Activity_Label", color="Gender", barmode="group", title="Physical Activity Level by Gender")
+st.plotly_chart(fig, use_container_width=True)
